fad823f7318a5529520a6d5456da9b4e424f3ca6
Paul <279141102@qq.com>
2017-05-19 15:52:38 +0800

    test

---
+++ src/test/Helloworld.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2017 Nokia. All rights reserved.
+ *
+ *  Revision History:
+ *
+ *  DATE/AUTHOR          COMMENT
+ *  ---------------------------------------------------------------------
+ *  2017年5月19日/juncli                            
+ */
+package test;
+
+/**
+ * TODO:Write class description
+ * @author <a HREF="mailto:junchao.li@nokia.com">Paul</a>
+ *
+ */
+public class Helloworld
+{
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        // TODO Auto-generated method stub
+        System.out.println( "Hello world!" );
+    }
+
+}
