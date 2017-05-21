3b70cf7ee9d55a9360c16cdbac90db0c34851cbc
Paul <279141102@qq.com>
2017-05-21 16:19:49 +0800

    branch

--- src/test/Helloworld.java
+++ src/test/Helloworld.java
@@ -15,6 +15,7 @@ public class Helloworld
     {
         // TODO Auto-generated method stub
         System.out.println( "Hello world!" );
+        System.out.println( "Hello world! Branch" );
     }
 
 }
